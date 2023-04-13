6af6dab3cbdbfe583862224a4426da7e17ed9f1c
William Wang <wmwang52@gmail.com>
2023-04-12 22:53:59 -0400

    getData is the most substantial portion of the program. This is as optimized as I can get it without spending far more time rehauling the basics of how selenium functions appropriately. It opens a Chrome tab, and clicks it on the page. There is an error with Lenoir's waffle bar due to CDS's website malfunctioning. Once it clicks the food link and gets the specific nutritional data, it parses through it with beautiful soup to get the necessary dietary data. It then assigns it to a food object and appends it to a list. We will need to add further error handling, and edge cases and consider how to slowly transition over to chrome windows in a more memory-efficient and faster way.

--- WebscrapingBackend.py
+++ WebscrapingBackend.py
@@ -74,3 +74,72 @@ def allFoodItemNames(pageLink):
             foodList = []
     driver.quit()
     return totalMealPeriodList
+
+
+# This method returns all the raw data given, and cleans the ingredient data and allergen data.
+def getData(foodItem, webpageLink, index):
+    fullData = []
+    fullData.append(foodItem)
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+
+    # Creates the web driver
+    driver = webdriver.Chrome(options=chrome_options)
+    try:
+        driver.get(webpageLink)
+        driver.find_elements(By.CLASS_NAME, "c-tabs-nav__link")[index].click()
+    except:
+        driver.quit()
+        raise ValueError("Invalid webpage link")
+
+    try:
+        driver.find_element(By.LINK_TEXT, foodItem).click()
+    except:
+        driver.quit()
+        raise ValueError(f"Invalid food item: {foodItem}")
+
+    time.sleep(0.25)
+
+    try:
+        NutritionData = driver.find_element(By.CLASS_NAME, "nutrition-facts-table").get_attribute("innerHTML")
+
+        soup = BeautifulSoup(NutritionData, 'html.parser')
+        NutritionList = []
+        for row in soup.find_all('tr'):
+            cells = row.find_all('th')
+            NutritionList.append(cells[0].get_text().replace("\n", "").strip())
+
+        fullData.append(NutritionList[0])
+        for index in range(1, len(NutritionList)):
+            tempValue = NutritionList[index].split("       ")
+            tempValue[0] = tempValue[0].strip()
+            tempValue[1] = tempValue[1].strip()
+            NutritionList[index] = f"{tempValue[0]}:{tempValue[1]}"
+            fullData.append(NutritionList[index])
+
+        extraRawData = driver.find_elements(By.CSS_SELECTOR, "p")
+    except:
+        exit(f"{foodItem}")
+
+    if "Ingredients" in extraRawData[0].get_attribute("innerHTML"):
+        soup = BeautifulSoup(extraRawData[0].get_attribute("innerHTML"), 'html.parser')
+        fullData.append(soup.get_text().title())
+    else:
+        soup = BeautifulSoup(extraRawData[1].get_attribute("innerHTML"), 'html.parser')
+        fullData.append(soup.get_text().title())
+        fullData.append(f"Allergens: {extraRawData[0].get_attribute('innerHTML')}")
+
+    driver.quit()
+
+    print(fullData)
+    if "Ingredients" in fullData[-1]:
+
+        foodItemNew = Food(fullData[0], fullData[1], fullData[2], fullData[3], fullData[4], fullData[5], fullData[6],
+                           fullData[7], fullData[8], fullData[9], fullData[10], fullData[11], fullData[12],
+                           fullData[13])
+    else:
+        foodItemNew = Food(fullData[0], fullData[1], fullData[2], fullData[3], fullData[4], fullData[5], fullData[6],
+                           fullData[7], fullData[8], fullData[9], fullData[10], fullData[11], fullData[12],
+                           fullData[13],
+                           fullData[14])
+    return foodItemNew
